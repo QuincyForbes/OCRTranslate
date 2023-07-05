1a631f913d394739c29c5c701fb08c2c7759377f
QuincyForbes <quincyforbes@pm.me>
2023-07-05 13:59:10 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+import engine as en
+
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "uploads/"
+app.config["OUTPUT_FOLDER"] = "outputs/"
+
+default_prompt = en.config["DefaultPrompt"]
+
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+
+@app.route("/outputs/<filename>")
+def output_file(filename):
+    return send_from_directory(app.config["OUTPUT_FOLDER"], filename)
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files.get("file")
+        current_prompt = request.form.get("promptbox")
+        if not file:
+            return render_template("upload.html", default_prompt=default_prompt)
+
+        if file:
+            print(current_prompt)
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath)
+            processed_name, detected_texts = en.execute(filepath, default_prompt)
+            processed_filepath = os.path.join(
+                app.config["OUTPUT_FOLDER"], processed_name
+            )
+
+            return render_template(
+                "display.html",
+                orig_image=filepath,
+                proc_image=processed_filepath,
+                texts=detected_texts,
+                default_prompt=default_prompt,
+            )
+
+    return render_template("upload.html", default_prompt=default_prompt)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
